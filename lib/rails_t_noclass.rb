ef427fdbd9a6dd5702d19bcbf80f656bd3c426a4
ISHIKAWA Takayuki <topstone@users.noreply.github.com>
2025-11-29 22:34:12 +0900

    bundle gem

---
+++ lib/rails_t_noclass.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_t_noclass/version"
+
+module RailsTNoclass
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rails_t_noclass/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RailsTNoclass
+  VERSION = "0.1.0"
+end
