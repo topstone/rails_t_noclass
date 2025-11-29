453c1d7cd1077c6b605d96aca9b10d4745848b27
ISHIKAWA Takayuki <topstone@users.noreply.github.com>
2025-11-29 22:57:28 +0900

    rubocop-rake, rubocop-rspec を追加

--- Gemfile
+++ Gemfile
@@ -11,5 +11,7 @@ gem "rake"
 gem "rspec"
 
 gem "rubocop"
+gem "rubocop-rake"
+gem "rubocop-rspec"
 
 gem "yard"
--- spec/rails_t_noclass_spec.rb
+++ spec/rails_t_noclass_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe RailsTNoclass do
   it "has a version number" do
-    expect(RailsTNoclass::VERSION).not_to be nil
+    expect(RailsTNoclass::VERSION).not_to be_nil
   end
 
   # it "does something useful" do
